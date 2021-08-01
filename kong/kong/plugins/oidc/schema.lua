04a998b1e7e4723db9c2fad600a94dcc50fdbcb6
jenkins <jenkins@epam.com>
2021-08-01 00:00:00 +0000

    [EDP-DDM-0000] Codebase GitHub migration

---
+++ kong/kong/plugins/oidc/schema.lua
@@ -0,0 +1,32 @@
+return {
+  no_consumer = true,
+  fields = {
+    client_id = { type = "string", required = true },
+    client_secret = { type = "string", required = true },
+    discovery = { type = "string", required = true, default = "https://.well-known/openid-configuration" },
+    introspection_endpoint = { type = "string", required = false },
+    timeout = { type = "number", required = false },
+    introspection_endpoint_auth_method = { type = "string", required = false },
+    bearer_only = { type = "string", required = true, default = "no" },
+    realm = { type = "string", required = true, default = "kong" },
+    redirect_uri_path = { type = "string" },
+    --redirect_uri = { type = "string" },
+    scope = { type = "string", required = true, default = "openid" },
+    response_type = { type = "string", required = true, default = "code" },
+    ssl_verify = { type = "string", required = true, default = "no" },
+    token_endpoint_auth_method = { type = "string", required = true, default = "client_secret_post" },
+    session_secret = { type = "string", required = false },
+    recovery_page_path = { type = "string" },
+    logout_path = { type = "string", required = false, default = '/logout' },
+    redirect_after_logout_uri = { type = "string", required = false },
+    filters = { type = "string" },
+    unauth_action = { type = "string", required = true, default = "deny" },
+    access_token_header_name = { type = "string", required = true, default = "X-Access-Token" },
+    bearer_access_token = { type = "string", required = true, default = "no" },
+    id_token_header_name = { type = "string", required = true, default = "X-ID-Token" },
+    user_header_name = { type = "string", required = true, default = "X-Userinfo" },
+    revoke_tokens_on_logout = {type = "boolean", required = false, default = false},
+    session_opts = { type = "string"},
+    post_logout_redirect_uri = { type = "string", required = false}
+  }
+}
